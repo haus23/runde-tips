8b0458f8144d0c28c815ba589cf01296c4171406
Micha Buchholz <micha@haus23.net>
2024-01-06 22:06:54 +0100

    fix: Remove useLayoutEffect warning. Intro to framer motion as well.

--- app/components/header/app-header.tsx
+++ app/components/header/app-header.tsx
@@ -1,15 +1,19 @@
-import { useWindowScroll } from '@uidotdev/usehooks';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import { Logo } from './logo/logo';
 
 export function AppHeader() {
-  const [{ y: scrolled }] = useWindowScroll();
+  const { scrollY } = useScroll();
 
   return (
-    <div
-      {...(!scrolled && { 'data-top': true })}
-      className="fixed inset-x-0 top-0 h-14 flex items-center px-2 sm:px-4 border-b data-[top]:border-b-0 bg-gray-50"
+    <motion.header
+      style={{
+        borderBottomColor: useTransform(scrollY, (v) =>
+          Number(!v) ? 'var(--mauve-1)' : 'var(--mauve-6)',
+        ),
+      }}
+      className="bg-gray-50 fixed inset-x-0 top-0 h-14 flex items-center px-2 sm:px-4  border-b"
     >
       <Logo />
-    </div>
+    </motion.header>
   );
 }
