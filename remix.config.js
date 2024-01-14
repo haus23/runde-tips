668f8224b0db05d86db3a05719f78b54e6f96a27
Micha Buchholz <mail@lean-stack.de>
2024-01-14 17:44:44 +0100

    feat: Switch to flat routes. Resolves #10

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,7 @@
+import { flatRoutes } from 'remix-flat-routes';
+
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ['**/.*'],
+  ignoredRouteFiles: ['**/*'],
+  routes: async (defineRoutes) => flatRoutes('routes', defineRoutes),
 };
