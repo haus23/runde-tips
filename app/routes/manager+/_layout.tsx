bcd519b7bcc13cff0f6f5d6e3c3ebc94286f12e7
Micha Buchholz <mail@lean-stack.de>
2024-01-25 21:09:44 +0100

    fix: Keep border - but transparent.

--- app/routes/manager+/_layout.tsx
+++ app/routes/manager+/_layout.tsx
@@ -15,7 +15,7 @@ export default function ManagerLayout() {
     <div>
       <div className="hidden bg-panel md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
         <div className="flex flex-col">
-          <div className="h-14 px-2 sm:px-4 flex items-center border-b border-neutral">
+          <div className="h-14 px-2 sm:px-4 flex items-center border-b border-transparent">
             <Link to="/" className="pl-1 pr-2 w-full" variant="primary">
               <Logo />
             </Link>
